113de4b806128bd27ae721d5c0f8f7864409de90
Eric 张哲涵 <github@zhehanz.com>
2023-12-07 17:57:44 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -25,8 +25,8 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 'https://zhehanz.com/page-icon.png',
-  defaultPageCover: 'https://zhehanz.com/page-cover.jpg',
+  defaultPageIcon: null,
+  defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
