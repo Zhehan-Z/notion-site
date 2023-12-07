186afa80209957ebb31c56852425e265c21d1ec1
Eric 张哲涵 <github@zhehanz.com>
2023-12-07 17:51:39 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,32 +2,31 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'c406c2582d8e49a59ec81256ec06e63b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'ZhehanZ',
+  domain: 'zhehanz.com',
+  author: 'Zhehan Zhang',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'The personal site of Zhehan Zhang. Get to know me more on this site!',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  twitter: 'Zhehan_Z',
+  github: 'Zhehan-Z',
+  // linkedin: 'mighil',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
+  defaultPageIcon: 'https://zhehanz.com/page-icon.png',
+  defaultPageCover: 'https://zhehanz.com/page-cover.jpg',
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
@@ -42,24 +41,33 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+     '/blog': '68807bbb3db746e6ab6b7db79477aadf',
+     '/about': 'b51d52be62784a6d83ed71866ae26b11',
+     '/friends': '5d9008b017d248e492b79ce96b95118b',
+     '/contact': 'b9451aab2a3f48c89839c28074d66f87',
+   },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [   
+    {
+      title: 'Blog',
+      pageId: '68807bbb3db746e6ab6b7db79477aadf'
+    },    
+    {
+      title: 'About',
+      pageId: 'b51d52be62784a6d83ed71866ae26b11'
+    },
+    {
+      title: 'Friends',
+      pageId: '5d9008b017d248e492b79ce96b95118b'
+    },
+    {
+      title: 'Contact',
+      pageId: 'b9451aab2a3f48c89839c28074d66f87'
+    }
+  ]
 })
